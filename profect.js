057c613dfa9df4f3f2de7944f514a14ce6cdd47c
Shijil <shijilcg@gmail.com>
2024-08-13 14:03:30 +0530

    last change

--- profect.js
+++ profect.js
@@ -3,7 +3,8 @@ let value =0;
 let intervalId;
 let intervalId2;
 let flag=0;
-let flag2=0
+let flag2=0;
+let flag3=0;
 function starttheflow() {
   flag = 1;
   if(delytime!=0 && value!=0 && flag2===0){ 
@@ -83,14 +84,15 @@ function chandedtheline(event) {
      }
 }
 function chandedthetime(event) {
-  //  if(flag2===0){
+   if(flag3===0){
     clearInterval(intervalId);
     clearInterval(intervalId2);
     delytime=event.target.value;
+    flag=1
     starttheflow();
-  //  }else{
-  //     console.log("Please click stop before change time");
-  //  }
+   }else{  
+    delytime=event.target.value;
+   }
   
 }
 
