b44b855b29314a631cf0860768ce023d827fecbc
Shijil <shijilcg@gmail.com>
2024-08-13 14:47:37 +0530

    last and final stage

--- profect.js
+++ profect.js
@@ -3,34 +3,38 @@ let value =0;
 let intervalId;
 let intervalId2;
 let flag=0;
-let flag2=0;
 let flag3=0;
+let flag2=0;
 function starttheflow() {
+  flag3=1;
   flag = 1;
-  if(delytime!=0 && value!=0 && flag2===0){ 
-     flag2=1;
+  if(delytime!=0 && value!=0 && flag2==0){ 
+      flag2=1;
      let items=document.getElementsByClassName('maincontent');
      let index = 0;
+
     if (items.length === 0) return; 
+
     if(items.length === 1){
+
       let div=document.getElementById('circular')
       intervalId2 = setInterval(() => {
         div.style.backgroundColor = (div.style.backgroundColor === 'red' || div.style.backgroundColor === '') ? 'white' : 'red';
-        console.log(delytime);
       }, delytime);
+
     }else{
+
      intervalId = setInterval(() => {
       for(let item of items){
         let children=item.children;
        for(let child of children){
-        console.log("change color white");
           child.style.background='white';
        }
       }
       makeblink(items[index]);
       index = (index + 1) % items.length; 
-      console.log(delytime);
     },delytime); 
+
   }
 }
 else{
@@ -40,7 +44,6 @@ else{
  
 function makeblink(item){
 
-   console.log("change color red");
      let children=item.children;
      for(let child of children){
         child.style.background='red';
@@ -53,7 +56,6 @@ function stoptheflow() {
   let items=document.getElementsByClassName('maincontent');
   if(delytime!=0 && value!=0){ 
     if(items.length===1){
-      console.log("ihhihj");
       clearInterval(intervalId2);
     }else{
       clearInterval(intervalId);
@@ -83,16 +85,15 @@ function chandedtheline(event) {
         console.log("\n")
      }
 }
-function chandedthetime(event) {
-   if(flag3 == 0){
+function chandedthetime(event) { 
+  if(flag3 == 0){
+    delytime=event.target.value;
+  }else{
+    flag2=0;
     clearInterval(intervalId);
     clearInterval(intervalId2);
     delytime=event.target.value;
-    flag=1
     starttheflow();
-   }else{  
-    delytime=event.target.value;
-   }
-  
+  }
 }
 
